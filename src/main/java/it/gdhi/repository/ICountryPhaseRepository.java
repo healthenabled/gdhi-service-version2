a564ed4092cbe4797291403c9290ef9f36e77ad5
ashutoshroy <ashutoshroywork@gmail.com>
2023-01-24 21:34:45 +0530

    GDHI-Tech [Refactoring] Update Crud to Jpa Repository. <Ashutosh/Arjun>

--- src/main/java/it/gdhi/repository/ICountryPhaseRepository.java
+++ src/main/java/it/gdhi/repository/ICountryPhaseRepository.java
@@ -1,9 +1,9 @@
 package it.gdhi.repository;
 
 import it.gdhi.model.CountryPhase;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICountryPhaseRepository extends CrudRepository<CountryPhase, String> {
+public interface ICountryPhaseRepository extends JpaRepository<CountryPhase, String> {
 }
