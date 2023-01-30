2d4aeacb4f3ecdfe70159865fe055432dbbeaac1
ashutoshroy <ashutoshroywork@gmail.com>
2023-01-30 13:14:34 +0530

    GDHI-Tech [Refactoring] Remove unused methods and @Query annotation from IDevelopmentIndicatorRepository.java. <Ashutosh/Arjun>

--- src/main/java/it/gdhi/repository/IDevelopmentIndicatorRepository.java
+++ src/main/java/it/gdhi/repository/IDevelopmentIndicatorRepository.java
@@ -2,18 +2,11 @@ package it.gdhi.repository;
 
 import it.gdhi.model.DevelopmentIndicator;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 @Repository
 public interface IDevelopmentIndicatorRepository extends JpaRepository<DevelopmentIndicator, Long> {
 
-    List<DevelopmentIndicator> findAll();
-
-    DevelopmentIndicator save(DevelopmentIndicator developmentIndicator);
-
-    @Query("SELECT d FROM DevelopmentIndicator d WHERE d.countryId = ?1")
     Optional<DevelopmentIndicator> findByCountryId(String countryCode);
 }
