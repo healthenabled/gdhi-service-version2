bc9e3931ff1a7d08bd66607b5c9f4e6dce9cf150
haikyuu77 <arjunsure.mec@gmail.com>
2023-02-06 12:39:52 +0530

    GDHI-Tech [14] Update indicator score definition for language arabic <Arjun/Preeti>

--- src/test/java/it/gdhi/internationalization/repository/IScoreDefinitionTranslationRepositoryTest.java
+++ src/test/java/it/gdhi/internationalization/repository/IScoreDefinitionTranslationRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
 
+import static it.gdhi.utils.LanguageCode.ar;
 import static it.gdhi.utils.LanguageCode.fr;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,6 +27,14 @@ public class IScoreDefinitionTranslationRepositoryTest {
         String expectedScoreDefinition = "La structure de gouvernance est entièrement fonctionnelle, dirigée par le gouvernement, consulte d'autres ministères et surveille la mise en œuvre de la santé numérique et de la gouvernance des données, y compris l'intelligence artificielle, sur la base d'un plan de travail.";
 
         String scoreDefinitionFR = repository.findTranslationForLanguage(fr, 1, 4);
+
+        assertEquals(expectedScoreDefinition, scoreDefinitionFR);
+    }
+    @Test
+    public void shouldReturnScoreDefinitionInArabicForGivenHealthIndicator() {
+        String expectedScoreDefinition = "يعمل هيكل الحوكمة بكامل طاقته، وتقوده الحكومة، ويتشاور مع الوزارات الأخرى، ويراقب تنفيذ الصحة الرقمية وحوكمة البيانات، بما في ذلك الذكاء الاصطناعي، ويتم كل ذلك بناءا على خطة عمل.";
+
+        String scoreDefinitionFR = repository.findTranslationForLanguage(ar, 1, 4);
 
         assertEquals(expectedScoreDefinition, scoreDefinitionFR);
     }
