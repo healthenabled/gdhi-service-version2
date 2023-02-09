e16aaba26665b38384de7a5c2c4f24fb6c6d2bc8
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-09 12:50:25 +0530

    GDHI-Tech [Refactoring] Load env variables during application start. <Ashutosh>

--- install_service_dependencies.sh
+++ install_service_dependencies.sh
@@ -2,7 +2,5 @@
 chmod +x ./gradlew
 echo "Show the current directory for execution."
 pwd
-echo "Added env vairables for QA."
-source ~/.bash_profile
 echo "Install dependencies for Spring Boot."
 ./gradlew clean build -x test
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source ~/.bash_profile
 ./gradlew bootRun --args='--spring.profiles.active=qa'
 
