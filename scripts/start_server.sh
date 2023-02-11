611449597c7e2343e380d0c9fecd9087fdafa1ff
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-11 10:12:44 +0530

    GDHI-Tech [Refactoring] Update script to close stdout and stderr streams.<Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 set -xe
 pwd
 source /opt/export_QA_env.sh
-nohup ./gradlew bootRun --args='--spring.profiles.active=qa' &
+nohup ./gradlew bootRun --args='--spring.profiles.active=qa' > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
 
