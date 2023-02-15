c4a037298bb2d7fd1193a2efcb1f77a340c41c2c
Ashutosh Roy <44959785+ashutosh-roy@users.noreply.github.com>
2023-02-15 16:34:27 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 pwd
-source /opt/export_QA_env.sh
+source /opt/scripts/export_QA_env.sh
 nohup ./gradlew bootRun --args='--spring.profiles.active=qa' > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
 
