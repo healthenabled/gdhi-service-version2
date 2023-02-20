98a92bdb0cb482d7a363f4f920e6e97839882dea
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-20 15:20:23 +0530

    GDHI-Tech [Refactoring] Add env vars for AWS. <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 pwd
-
+source /opt/scripts/export_aws_vars.sh
 FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_BACKEND --recursive | sort | tail -n 1 | awk '{print $4}'`
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE"  /opt/codedeploy-agent/
 
