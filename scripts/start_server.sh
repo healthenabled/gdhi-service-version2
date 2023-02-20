ea6bd7e005ecde4f202ef795a85f9cf8145e5153
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-20 14:40:03 +0530

    GDHI-Tech [Refactoring] Resolve merge conflicts. <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,14 @@
 set -xe
 pwd
 source /opt/scripts/export_QA_env.sh
-nohup ./gradlew bootRun --args='--spring.profiles.active=qa' > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
+FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_BACKEND --recursive | sort | tail -n 1 | awk '{print $4}'`
+/usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE"  /opt/codedeploy-agent/
 
+if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-QA" ]
+then
+    nohup java -jar *-all.jar -Dspring.profiles.active=qa > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
+fi
+if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-Showcase" ]
+then
+    nohup java -jar *-all.jar -Dspring.profiles.active=showcase > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
+fi
