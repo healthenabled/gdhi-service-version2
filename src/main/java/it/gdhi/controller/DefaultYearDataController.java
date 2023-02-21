e628a12b9ce9291018a76797b43e8f90b2fcfe6b
haikyuu77 <arjunsure.mec@gmail.com>
2023-02-21 15:53:50 +0530

    GDHI-33 Update defaultYearDataController with new post api and controller annotations <Arjun>

--- src/main/java/it/gdhi/controller/DefaultYearDataController.java
+++ src/main/java/it/gdhi/controller/DefaultYearDataController.java
@@ -1,11 +1,17 @@
 package it.gdhi.controller;
 
 import it.gdhi.service.DefaultYearDataService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@Slf4j
 public class DefaultYearDataController {
 
     private DefaultYearDataService defaultYearDataService;
@@ -19,4 +25,10 @@ public class DefaultYearDataController {
     public List<String> getAllYears() {
         return defaultYearDataService.fetchYears();
     }
+
+    @PostMapping("/default_year/submit")
+    public void saveDefaultYear(@RequestBody String defaultYear) {
+        defaultYearDataService.saveNewYear(defaultYear);
+    }
+
 }
