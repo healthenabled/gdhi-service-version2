fd77304cbb551d4ec623dfa7540f28ee4db4cea8
haikyuu77 <arjunsure.mec@gmail.com>
2023-02-21 20:21:14 +0530

    GDHI-33 Remove unused import from IDefaultYearData repository <Arjun>

--- src/main/java/it/gdhi/repository/IDefaultYearData.java
+++ src/main/java/it/gdhi/repository/IDefaultYearData.java
@@ -1,6 +1,5 @@
 package it.gdhi.repository;
 
-import it.gdhi.model.CountryPhase;
 import it.gdhi.model.DefaultYearData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
