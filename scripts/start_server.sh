b2297549bf8e6a6abd31d7240b9c535fc2cc0e35
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-22 16:57:20 +0530

    GDHI-Tech [Refactoring] Add executable permission to the java jar. <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,7 +6,7 @@ source /opt/scripts/export_aws_vars.sh
 rm -rf *.jar
 FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_BACKEND --recursive | sort | tail -n 1 | awk '{print $4}'`
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE"  /opt/codedeploy-agent/
-
+chmod +x gdhi*.jar
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-QA" ]
 then
     source /opt/scripts/export_QA_env.sh
