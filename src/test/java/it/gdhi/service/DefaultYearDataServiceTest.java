e31fb4ce3af90b0e918ee8da988b30be302188e2
haikyuu77 <arjunsure.mec@gmail.com>
2023-02-22 11:28:31 +0530

    GDHI-33 Refactor test to remove unused imports and code formattting <Arjun/Kaveri>

--- src/test/java/it/gdhi/service/DefaultYearDataServiceTest.java
+++ src/test/java/it/gdhi/service/DefaultYearDataServiceTest.java
@@ -7,47 +7,35 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static java.util.Arrays.asList;
-import org.springframework.util.Assert;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.codehaus.groovy.runtime.InvokerHelper.asList;
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class DefaultYearDataServiceTest {
 
-   @Mock
-   private IDefaultYearData iDefaultYearData;
+    @Mock
+    private IDefaultYearData iDefaultYearData;
 
-   @InjectMocks
-   private DefaultYearDataService defaultYearDataService;
+    @InjectMocks
+    private DefaultYearDataService defaultYearDataService;
 
-   @Test
-   public void shouldReturnAllYears(){
-       DefaultYearData year2021 = DefaultYearData.builder().year("2021").build();
-       DefaultYearData year2020 = DefaultYearData.builder().year("2020").build();
+    @Test
+    public void shouldReturnAllYears() {
+        DefaultYearData year2021 = DefaultYearData.builder().year("2021").build();
+        DefaultYearData year2020 = DefaultYearData.builder().year("2020").build();
 
-       when(iDefaultYearData.findAll()).thenReturn(asList(year2021,year2020));
-       List<String> expectedYears = Arrays.asList("2021","2020");
-       List<String> actualYears = defaultYearDataService.fetchYears();
+        when(iDefaultYearData.findAll()).thenReturn(asList(year2021, year2020));
+        List<String> expectedYears = Arrays.asList("2021", "2020");
+        List<String> actualYears = defaultYearDataService.fetchYears();
 
-       assertEquals(expectedYears,actualYears);
-   }
+        assertEquals(expectedYears, actualYears);
+    }
 
-//   @Test
-//    public void shouldSaveYear(){
-//       String year = "2022";
-//       DefaultYearData mock = mock(DefaultYearData.class);
-//
-//       doNothing().when(iDefaultYearData).save(mock);
-//       defaultYearDataService.saveNewYear(mock.getYear());
-//
-//       verify(iDefaultYearData).save(mock);
-//   }
     @Test
     public void shouldReturnLatestDefaultYear() {
         DefaultYearData year2021 = DefaultYearData.builder().year("2021").build();
@@ -56,6 +44,6 @@ public class DefaultYearDataServiceTest {
         String expectedDefaultYear = "2021";
         String actualDefaultYear = defaultYearDataService.fetchDefaultYear();
 
-        assertEquals(expectedDefaultYear,actualDefaultYear);
+        assertEquals(expectedDefaultYear, actualDefaultYear);
     }
 }
