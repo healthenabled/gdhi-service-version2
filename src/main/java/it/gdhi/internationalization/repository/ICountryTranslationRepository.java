c82750fde62099ad16849b9a71bcb39f7aa9b207
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-22 15:35:12 +0530

    GDHI-Tech [Refactoring] Fix autowiring issues for Internalisation repositories. <Ashutosh>

--- src/main/java/it/gdhi/internationalization/repository/ICountryTranslationRepository.java
+++ src/main/java/it/gdhi/internationalization/repository/ICountryTranslationRepository.java
@@ -1,11 +1,13 @@
 package it.gdhi.internationalization.repository;
 
 import it.gdhi.internationalization.model.CountryTranslation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ICountryTranslationRepository extends Repository<CountryTranslation, Long> {
+@Repository
+public interface ICountryTranslationRepository extends JpaRepository<CountryTranslation, Long> {
 
     @Query("SELECT c.name FROM CountryTranslation c " +
             "WHERE c.id.languageId = :languageId and c.id.countryId = :countryId")
--- src/main/java/it/gdhi/internationalization/service/CountryNameTranslator.java
+++ src/main/java/it/gdhi/internationalization/service/CountryNameTranslator.java
@@ -14,6 +14,7 @@ import static java.util.stream.Collectors.toList;
 @Component
 public class CountryNameTranslator {
 
+    @Autowired
     private final ICountryTranslationRepository translationRepository;
 
     @Autowired
