09aa849ae2e41b30ac8ad2cc5a520dbcc81e68e7
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-22 16:51:02 +0530

    GDHI-Tech [Refactoring] Update java running command. <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,17 +3,17 @@ set -xe
 pwd
 source /opt/scripts/export_aws_vars.sh
 # Remove existing jars
-rm -rf .jar
+rm -rf *.jar
 FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_BACKEND --recursive | sort | tail -n 1 | awk '{print $4}'`
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE"  /opt/codedeploy-agent/
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-QA" ]
 then
     source /opt/scripts/export_QA_env.sh
-    nohup java -jar *-all.jar -Dspring.profiles.active=qa > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
+    nohup java -jar gdhi-*.jar --spring.config.location=${SPRING_CONFIG_LOCATION} > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
 fi
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-Showcase" ]
 then
     source /opt/scripts/export_Showcase_env.sh
-    nohup java -jar *-all.jar -Dspring.profiles.active=showcase > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
+    nohup java -jar gdhi-*.jar --spring.config.location=${SPRING_CONFIG_LOCATION} > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
 fi
