cd7874ecbb356b8e473c0387d7f036227ded354a
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-23 21:39:33 +0530

    GDHI-Tech [Refactoring] Update start and stop server. <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ source /opt/scripts/export_aws_vars.sh
 rm -rf *.jar
 FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_BACKEND --recursive | sort | tail -n 1 | awk '{print $4}'`
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE" $CURRENT_DIR/
-chmod +x $CURRENT_DIR/$APPLICATION_NAME;
+chmod +x $CURRENT_DIR/gdhi-*.jar;
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-QA" ]
 then
     source /opt/scripts/export_QA_env.sh
@@ -16,6 +16,6 @@ then
 fi
 
 echo -n $"Starting ${APPLICATION_NAME}: "
-nohup java -jar $CURRENT_DIR/$APPLICATION_NAME --spring.config.location=${SPRING_CONFIG_LOCATION} > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
+nohup java -jar $CURRENT_DIR/gdhi-*.jar --spring.config.location=${SPRING_CONFIG_LOCATION} > /opt/logs/gdhi_stdout.txt 2> /opt/logs/gdhi_stderr.txt < /dev/null &
 PID="$!"
 echo "Started, pid: $PID"
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,4 +3,4 @@ set -xe
 pwd
 source /opt/scripts/export_aws_vars.sh
 echo -n $"Shutting down :"$APPLICATION_NAME
-pkill -f 'java.*$APPLICATION_NAME'
+pkill -f java
