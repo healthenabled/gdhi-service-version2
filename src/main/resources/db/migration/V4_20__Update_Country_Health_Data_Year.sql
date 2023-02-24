fcaf95333e66ad93d75eb9cba58c9f0818ae3a11
haikyuu77 <arjunsure.mec@gmail.com>
2023-02-24 15:09:54 +0530

    GDHI-34 Update migration script V4_20 <Arjun/Lahari>

--- src/main/resources/db/migration/V4_20__Update_Country_Health_Data_Year.sql
+++ src/main/resources/db/migration/V4_20__Update_Country_Health_Data_Year.sql
@@ -2,6 +2,6 @@ UPDATE country_health_data.health_indicators SET year = 'Version1' WHERE status 
 UPDATE country_health_data.country_summary SET year = 'Version1' WHERE status = 'PUBLISHED';
 UPDATE country_health_data.country_resource_links SET year = 'Version1' WHERE status = 'PUBLISHED';
 
-UPDATE country_health_data.health_indicators SET year = date_part('year', created_at) where status = 'DRAFT';
-UPDATE country_health_data.country_summary SET year = date_part('year', created_at) WHERE status = 'DRAFT';
-UPDATE country_health_data.country_resource_links SET year = date_part('year', created_at) WHERE status = 'DRAFT';
+UPDATE country_health_data.health_indicators SET year = date_part('year', created_at) WHERE status  != 'PUBLISHED';
+UPDATE country_health_data.country_summary SET year = date_part('year', created_at) WHERE status != 'PUBLISHED';
+UPDATE country_health_data.country_resource_links SET year = date_part('year', created_at) WHERE status != 'PUBLISHED';
