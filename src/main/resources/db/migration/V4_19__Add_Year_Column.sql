1a82f1fbf988afc49cf553f13b7edb18d628d828
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-03-06 14:47:30 +0530

    GDHI-34 Add if not exists to v4_19 migration script <Lahari/Arjun>

--- src/main/resources/db/migration/V4_19__Add_Year_Column.sql
+++ src/main/resources/db/migration/V4_19__Add_Year_Column.sql
@@ -1,5 +1,3 @@
-ALTER TABLE country_health_data.health_indicators ADD year VARCHAR;
-ALTER TABLE country_health_data.country_summary ADD year VARCHAR;
-ALTER TABLE country_health_data.country_resource_links ADD year VARCHAR;
-
-
+ALTER TABLE country_health_data.health_indicators ADD COLUMN IF NOT EXISTS year VARCHAR;
+ALTER TABLE country_health_data.country_summary ADD COLUMN IF NOT EXISTS year VARCHAR;
+ALTER TABLE country_health_data.country_resource_links ADD COLUMN IF NOT EXISTS year VARCHAR;
