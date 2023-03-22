1ede79637ef271537bd086188dfa0bab909f3567
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-03-22 17:03:26 +0530

    GDHI-22 Insert health indicators and score definitions for arabic <Lahari/Ashutosh>

---
+++ src/main/resources/db/migration/V4_49__Insert_i18n_Score_Definition_ar.sql
@@ -0,0 +1,69 @@
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 1,  'ar', 'لا تتضمن سياسات التحول الرقمي وإدارة البيانات الوطنية الفوائد والمخاطر المحتملة على الصحة العامة أو النتائج الصحية على الأفراد.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 2,  'ar', 'تشمل سياسات التحول الرقمي وحوكمة البيانات الوطنية الفوائد والمخاطر المحتملة على الصحة العامة أو النتائج الصحية على الأفراد، بشكل عشوائي.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 3,  'ar', 'تتضمن سياسات التحول الرقمي وإدارة البيانات الوطنية بشكل منهجي الفوائد والمخاطر المحتملة على الصحة العامة أو النتائج الصحية على الأفراد ولكن ليس لديها استراتيجية (استراتيجيات) لمعالجتها.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 4,  'ar', 'تتضمن سياسات التحول الرقمي وإدارة البيانات الوطنية بشكل منهجي الفوائد والمخاطر المحتملة على الصحة العامة أو النتائج الصحية على الأفراد ولديها بعض الاستراتيجيات لمعالجتها ولكن لم يتم تنفيذها بعد.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 5,  'ar', 'تراعي جميع سياسات التحول الرقمي وإدارة البيانات ذات الصلة بشكل كامل الفوائد والمخاطر المحتملة للأنظمة الصحية ومحددات الصحة والنتائج الصحية على الأفراد (بما في ذلك للأطفال والنساء والفئات الضعيفة) ولديها استراتيجيات متعددة القطاعات يتم تنفيذها بالكامل.');
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 1,  'ar', 'لا توجد خطة وطنية خاصة بالتكنولوجيات الناشئة (مثل الذكاء الاصطناعي والأجهزة القابلة للارتداء وسلسلة الكتل "البلوك تشين" وإنترنت الأشياء) لدعم أهداف الصحة العامة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 2,  'ar', 'تم وضع خطة وطنية خاصة بالتكنولوجيات الناشئة (مثل الذكاء الاصطناعي والأجهزة القابلة للارتداء وسلسلة الكتل "البلوك تشين" وإنترنت الأشياء) لدعم أهداف الصحة العامة، ولكنها لم تنفذ. ');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 3,  'ar', 'توجد خطة لتكنولوجيا ناشئة واحدة على الأقل (على سبيل المثال، الذكاء الاصطناعي، والأجهزة القابلة للارتداء ، وسلسلة الكتل "البلوك تشين"، وإنترنت الأشياء) لدعم أهداف الصحة العامة. وتحدد الخطة (الخطط) آليات الحوكمة المطلوبة للتكنولوجيات الناشئة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 4,  'ar', 'يجري تنفيذ خطة لتكنولوجيا ناشئة واحدة أو أكثر (على سبيل المثال، الذكاء الاصطناعي، والأجهزة القابلة للارتداء، وسلسلة الكتل "البلوك تشين"، وإنترنت الأشياء) لدعم الصحة العامة، ويتم تمويلها، ويتم رصد النتائج. وتتوفر الآليات المطلوبة لإدارة التكنولوجيا الناشئة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 5,  'ar', 'يجري تنفيذ خطة لتكنولوجيا ناشئة واحدة أو أكثر (على سبيل المثال، الذكاء الاصطناعي، والأجهزة القابلة للارتداء، وسلسلة الكتل "البلوك تشين"، وإنترنت الأشياء) لدعم الصحة العامة، ويتم تمويلها ورصد النتائج وتحديث الخطة باستمرار. وتتوفر وتستخدم آليات للإدارة.');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 1,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وبرامج الصحة الرقمية دون النظر إلى العدالة والآثار المترتبة على حقوق الإنسان.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 2,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وبرامج الصحة الرقمية مع مراعاة الآثار المترتبة على العدالة وحقوق الإنسان.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 3,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وبرامج الصحة الرقمية مع النظر رسميا في الآثار المترتبة على الإنصاف وحقوق الإنسان جوت أن تتوفر استراتيجية (استراتيجيات) لمعالجتها');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 4,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وبرامج الصحة الرقمية مع النظر رسميا في الآثار المترتبة على الإنصاف وحقوق الإنسان وتتوفر استراتيجية (استراتيجيات) مخصصة لمعالجتها.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 5,  'ar', 'تقوم البلد بتنفيذ وتقييم آثار استراتيجيات الصحة الرقمية وحلول الصحة الرقمية المحددة بناء على تقييمات الآثار المترتبة على الإنصاف وحقوق الإنسان. وهناك استراتيجيات موثقة لمعالجة الثغرات في الوصول والنتائج لمختلف الفئات السكانية، بما في ذلك النساء والأطفال والفئات المهمشة. ');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 1,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وتدخلات الصحة الرقمية دون مراعاة المعايير والأدوار والعلاقات المتصلة بالنوع الاجتماعي.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 2,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وتدخلات الصحة الرقمية مع مراعاة المعايير والأدوار والعلاقات المتصلة بالنوع الاجتماعي بشكل عشوائي وغير منهجي.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 3,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وبرامج الصحة الرقمية مع مراعاة منهجية للمعايير والأدوار والعلاقات المتصلة بالنوع الاجتماعي دون وجود سياسات أو هياكل للمساءلة (تراعي الفوارق بين الجنسين).');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 4,  'ar', 'يتم تطوير وتنفيذ استراتيجيات وبرامج الصحة الرقمية مع مراعاة منهجية للمعايير والأدوار والعلاقات المتصلة بالنوع الاجتماعي مع وجود استراتيجيات محددة تستهدف هيئات الحوكمة والقوى العاملة الصحية والعملاء بآليات للمساءلة (خاصة بالنوع الاجتماعي).');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 5,  'ar', 'تعالج استراتيجيات وسياسات وتدخلات الصحة الرقمية عدم المساواة بين الجنسين وتعزز التغيير التدريجي في ديناميكيات النوع الاجتماعي بناء على تحليلات النوع الاجتماعي الروتينية وتقييمات الأثر (تغيير المفاهيم بين الجنسين).');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 1,  'ar', 'استراتيجية الصحة الرقمية غير موجودة أو موجودة ولا تتماشى مع التغطية الصحية الشاملة ولا تتناول أيا من المكونات الأساسية للتغطية والوصول والاستيعاب والجودة والعدالة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 2,  'ar', 'توجد استراتيجية للصحة الرقمية وتتماشى جزئيا مع التغطية الصحية الشاملة، وتتناول مكونا أساسيا واحدا فقط من مكونات التغطية والوصول والاستيعاب والجودة والعدالة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 3,  'ar', 'توجد استراتيجية للصحة الرقمية وتتماشى جزئيا مع التغطية الصحية الشاملة، وتتناول مكونين أو أكثر من المكونات الأساسية للتغطية والوصول والاستيعاب والجودة والعدالة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 4,  'ar', 'توجد استراتيجية للصحة الرقمية وتتماشى تماما مع أهداف التغطية الصحية الشاملة للبلد، ولكنها لا تتضمن مقاييس لتقييم مساهمة الصحة الرقمية في تحقيق أهداف التغطية الصحية الشاملة.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 5,  'ar', 'تتماشى استراتيجية الصحة الرقمية تماما مع أهداف التغطية الصحية الشاملة للبلد وتتضمن مقاييس لتقييم مساهمة الصحة الرقمية في تحقيق أهداف التغطية الصحية الشاملة.');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 1,  'ar', 'لا يشارك القطاع الخاص بشكل واضح ولا يستثمر في أنشطة الصحة الرقمية في البلاد');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 2,  'ar', 'مشاركة القطاع الخاص واستثماره في أنشطة الصحة الرقمية في البلاد تتم بشكل عشوائي ومحدود.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 3,  'ar', 'مشاركة القطاع الخاص واستثماره في أنشطة الصحة الرقمية في البلاد منهجية ولكنها لا تلبي احتياجات البلاد.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 4,  'ar', 'مشاركة القطاع الخاص واستثماره في أنشطة الصحة الرقمية في البلاد منهجية وتلبي احتياجات البلاد.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 5,  'ar', 'مشاركة القطاع الخاص واستثماره في أنشطة الصحة الرقمية في البلاد تتم في بيئة مُثلى');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 1,  'ar', 'لا توجد بروتوكولات أو سياسات أو أطر أو عمليات مقبولة تحكم استخدام الذكاء الاصطناعي في الخدمات الصحية.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 2,  'ar', 'تم اقتراح بروتوكولات أو سياسات أو أطر أو عمليات مقبولة تحكم استخدام الذكاء الاصطناعي في الخدمات الصحية وهي قيد المراجعة');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 3,  'ar', 'تم إقرار البروتوكولات أو السياسات أو الأطر أو العمليات المقبولة التي تحكم استخدام الذكاء الاصطناعي في الخدمات الصحية ولكنها لم تنفذ بالكامل.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 4,  'ar', 'تم تطبيق البروتوكولات أو السياسات أو الأطر أو العمليات المقبولة التي تحكم استخدام الذكاء الاصطناعي في الخدمات الصحية ولكن لا يتم إنفاذها باستمرار.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 5,  'ar', 'تم تنفيذ البروتوكولات أو السياسات أو الأطر أو العمليات المقبولة التي تحكم استخدام الذكاء الاصطناعي في الخدمات الصحية ويتم إنفاذها باستمرار.');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 1,  'ar', 'لا يوجد نظام آمن لتلقي التغذية الراجعة من المرضى. ');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 2,  'ar', 'يوجد نظام آمن لتلقي التغذية الراجعة من المرضى لبعض الخدمات الصحية، ولكنه غير مكتمل / متاح جزئيا، ويتم استخدامه وصيانته بشكل غير منتظم.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 3,  'ar', 'يوجد نظام آمن لتلقي التغذية الراجعة، وهو متاح وقيد الاستخدام النشط ويتضمن بيانات من <25٪ من المؤسسات الصحية ذات الصلة و/ أو الموقع الجغرافي. وهو متاح لبعض السكان.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 4,  'ar', 'يوجد نظام آمن لتلقي التغذية الراجعة، وهو متاح وقيد الاستخدام النشط ويتضمن بيانات من 25-75٪ من المؤسسات الصحية ذات الصلة و/ أو الموقع الجغرافي. وهو متاح لمعظم السكان');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 5,  'ar', 'يوجد نظام آمن لتلقي التغذية الراجعة، وهو متاح بأشكال يسهل الوصول إليها وقيد الاستخدام النشط، ويتضمن بيانات من >75٪ من المؤسسات الصحية ذات الصلة و/أو الموقع الجغرافي. وهو متاح لجميع السكان.');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35,-1,  'ar', 'غير متاح');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 1,  'ar', 'لا توجد مساهمة لأي مبادرة تتعلق بالصحة الرقمية في إعداد التقارير الروتينية أو في اتخاذ القرارات القائمة على البيانات لإدارة صحة السكان');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 2,  'ar', 'تساهم الأنظمة الرقمية المستخدمة على مستوى المقاطعة / الولاية فقط في اعداد التقارير حول الصحة العامة في البلاد واتخاذ القرارات لإدارة صحة السكان.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 3,  'ar', 'تستخدم الأنظمة الرقمية على مستوى المرافق أو المجتمع وتساهم في اعداد التقارير حول الصحة العامة في البلاد واتخاذ القرارات لإدارة صحة السكان.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 4,  'ar', 'تستخدم الأنظمة الرقمية في المرافق أو المجتمعات ومن قبل الأفراد وتساهم في إعداد التقارير حول الصحة العامة في البلاد واتخاذ القرارات لإدارة صحة السكان.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 5,  'ar', 'تساهم الأنظمة الرقمية المستخدمة على جميع المستويات، بما في ذلك الأنظمة المستخدمة من قبل الأفراد، في إعداد التقارير حول الصحة العامة في الوقت المناسب واتخاذ القرارات المتعلقة بإدارة صحة السكان');
