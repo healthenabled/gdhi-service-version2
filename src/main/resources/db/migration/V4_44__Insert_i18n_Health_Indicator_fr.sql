93391c48ab7295c91245e50be6007963e9cd988e
ashutoshroy <ashutoshroywork@gmail.com>
2023-03-22 12:51:58 +0530

    GDHI-22 Insert new indicators in french. <Ashutosh/Lahari>

---
+++ src/main/resources/db/migration/V4_44__Insert_i18n_Health_Indicator_fr.sql
@@ -0,0 +1,26 @@
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(30 , 'fr' ,  'La santé est une priorité dans les politiques nationales de transformation numérique et de gouvernance des données.' ,
+'Les politiques et approches nationales de transformation numérique et de gouvernance des données prennent-elles en compte et traitent-elles les avantages et les risques potentiels pour la santé publique et la santé individuelle ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(31,  'fr', 'Préparation à l''adoption et à la gouvernance des technologies émergentes',
+'Existe-t-il un plan national spécifique aux technologies émergentes (par exemple, IA, Wearables, Blockchain, IoT) pour soutenir les objectifs de santé publique ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(32,  'fr', 'Analyse, planification et suivi de la diversité, de l''équité et des droits de l''homme inclus dans les stratégies et plans nationaux de santé numérique',
+'Le pays a-t-il évalué/adapté les stratégies nationales de santé numérique du point de vue de l''équité et des droits de l''homme ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(33,  'fr', 'Prise en compte des considérations de genre dans les stratégies de santé numérique et la gouvernance de la santé numérique',
+'Le pays intègre-t-il des considérations de genre dans la stratégie nationale de santé numérique ou sa gouvernance de la santé numérique ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(37,  'fr', 'Alignement de la stratégie numérique nationale sur les composantes essentielles de la couverture sanitaire universelle (CSU)',
+'La stratégie nationale de santé numérique du pays est-elle alignée sur la couverture sanitaire universelle et/ou les composantes essentielles de la CSU ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(38,  'fr', 'Participation et investissements du secteur privé dans la santé numérique',
+'Compte tenu de l''environnement favorable, le secteur privé participe-t-il et investit-il dans les activités de santé numérique ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(36,  'fr', 'Protocole de régulation et de certification de l''IA au sein des services de santé',
+'Existe-t-il des protocoles, des politiques, des cadres ou des processus acceptés régissant l''utilisation de l''IA dans les systèmes, services et applications de santé, en particulier en ce qui concerne l''éthique, l''équité, la sécurité, l''intégrité des données et la qualité des soins ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(34,  'fr', 'Sécuriser les systèmes de rétroaction des patients',
+'Plus précisément, existe-t-il un système sécurisé de retour d''information des patients, disponible, accessible ?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(35,  'fr', 'Contribution de la santé numérique à la gestion de la santé de la population',
+'Les initiatives nationales actuelles en matière de santé numérique contribuent-elles aux rapports et à la prise de décision en matière de santé publique ?');
+
+
+
+
+
+
+
+
---
+++ src/main/resources/db/migration/V4_45__Insert_i18n_Score_Definition_fr.sql
@@ -0,0 +1,69 @@
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 1,  'fr', 'Les politiques nationales de transformation numérique et de gouvernance des données n''incluent pas les avantages et les risques potentiels pour la santé publique ou les résultats de santé individuels.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 2,  'fr', 'Les politiques nationales de transformation numérique et de gouvernance des données incluent les avantages et les risques potentiels pour la santé publique ou les résultats de santé individuels sur une base ad hoc.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 3,  'fr', 'Les politiques nationales de transformation numérique et de gouvernance des données incluent systématiquement les avantages et les risques potentiels pour la santé publique ou les résultats de santé individuels, mais n''ont pas de stratégie(s) pour y faire face.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 4,  'fr', 'Les politiques nationales de transformation numérique et de gouvernance des données incluent systématiquement les avantages et les risques potentiels pour la santé publique ou les résultats de santé individuels et ont une ou plusieurs stratégies pour y faire face qui ne sont pas encore mises en œuvre.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 30, 5,  'fr', 'Toutes les politiques pertinentes de transformation numérique et de gouvernance des données tiennent pleinement compte des avantages et des risques potentiels pour les systèmes de santé, les déterminants de la santé et les résultats de santé individuels (y compris pour les enfants, les femmes et les groupes vulnérables) et disposent de stratégies multisectorielles pleinement mises en œuvre.');
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 1,  'fr', 'Il n''y a pas de plan de technologies émergentes (par exemple, IA, Wearables, Blockchain, IoT) à l''appui des objectifs de santé publique.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 2,  'fr', 'Un plan a été élaboré pour au moins une technologie émergente (par exemple, IA, Wearables, Blockchain, IdO) pour soutenir les objectifs de santé publique, mais il n''est pas mis en œuvre.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 3,  'fr', 'Un plan existe pour au moins une technologie émergente (par exemple, IA, Wearables, Blockchain, IoT) pour soutenir les objectifs de santé publique. Le ou les plans identifient les mécanismes de gouvernance requis pour les technologies émergentes.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 4,  'fr', 'Un plan pour une ou plusieurs technologies émergentes (par exemple, IA, Wearables, Blockchain, IoT) pour soutenir la santé publique est mis en œuvre, est financé et les résultats sont surveillés. Les mécanismes de gouvernance requis pour les technologies émergentes sont en place.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 31, 5,  'fr', 'Un plan pour une ou plusieurs technologies émergentes (par exemple, IA, Wearables, Blockchain, IoT) à l''appui de la santé publique est mis en œuvre, est financé, les résultats sont surveillés et le plan est tenu à jour. Des mécanismes de gouvernance sont en place et utilisés.');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 1,  'fr', 'Les stratégies et programmes de santé numérique sont élaborés et mis en œuvre sans tenir compte des implications en matière d''équité et de droits de l''homme.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 2,  'fr', 'Des stratégies et des programmes de santé numérique sont élaborés et mis en œuvre en tenant compte au cas par cas des implications en matière d''équité et de droits de l''homme.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 3,  'fr', 'Les stratégies et programmes de santé numérique sont élaborés et mis en œuvre en tenant compte formellement des implications en matière d''équité et de droits de l''homme, sans stratégie(s) pour y faire face.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 4,  'fr', 'Des stratégies et des programmes de santé numérique sont élaborés et mis en œuvre en tenant compte formellement des implications en matière d''équité et de droits de l''homme avec une ou des stratégies ad hoc pour y faire face.');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 32, 5,  'fr', 'Le pays met en œuvre et évalue les effets des stratégies de santé numérique et des solutions de santé numérique spécifiques basées sur des évaluations d''impact sur l''équité et les droits de l''homme. Des stratégies documentées sont en place pour combler les lacunes en matière d''accès et de résultats pour différents groupes de population, y compris les femmes, les enfants et les groupes marginalisés.');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 1,  'fr', '');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 2,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 3,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 4,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 33, 5,  'fr', 'Non disponible');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 2,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 3,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 4,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 37, 5,  'fr', 'Non disponible');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 2,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 3,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 4,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 38, 5,  'fr', 'Non disponible');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 2,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 3,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 4,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 36, 5,  'fr', 'Non disponible');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 2,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 3,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 4,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 34, 5,  'fr', 'Non disponible');
+
+
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35,-1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 1,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 2,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 3,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 4,  'fr', 'Non disponible');
+INSERT INTO i18n.score_definition(indicator_id, score, language_id, definition) VALUES( 35, 5,  'fr', 'Non disponible');
