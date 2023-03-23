a9622d2d6ac81a88682a27a6c25be15f4e329e31
ashutoshroy <ashutoshroywork@gmail.com>
2023-03-23 15:32:09 +0530

    GDHI-22 Add health indicators and score definitions in spanish. <Ashutosh/Lahari>

---
+++ src/main/resources/db/migration/V4_50__Insert_i18n_Health_Indicator_es.sql
@@ -0,0 +1,26 @@
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(30 , 'es' ,  'La salud es una prioridad en las políticas nacionales de transformación digital y gobernanza de datos.' ,
+'¿Las políticas y enfoques nacionales de transformación digital y gobernanza de datos consideran y abordan los posibles beneficios y riesgos para la salud pública y la salud individual?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(31,  'es', 'Disponibilidad para la adopción de tecnologías emergentes y gobernanza',
+'¿Existe un plan nacional específico para las tecnologías emergentes (p. ej., IA, Wearables, Blockchain, IoT) para apoyar los objetivos de salud pública?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(32,  'es', 'Análisis, planificación y seguimiento de la diversidad, la equidad y los derechos humanos incluidos en las estrategias y planes nacionales de salud digital',
+'¿El país ha evaluado/adaptado las estrategias nacionales de salud digital desde una perspectiva de equidad y derechos humanos?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(33,  'es', 'Consideraciones de género en las estrategias de salud digital y la gobernanza de la salud digital',
+'¿Incluye el país consideraciones de género en la estrategia nacional de salud digital o en su gobernanza de la salud digital?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(37,  'es', 'Alineación de la estrategia digital nacional con los componentes básicos de la cobertura sanitaria universal (CSU)',
+'¿La estrategia nacional de salud digital del país está alineada con la cobertura universal de salud y/o los componentes básicos de la CSU?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(38,  'es', 'Participación e inversiones del sector privado en salud digital',
+'Dado el entorno propicio, ¿el sector privado participa e invierte en actividades de salud digital?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(36,  'es', 'Protocolo para regular y certificar la IA en los servicios de salud',
+'¿Existen protocolos, políticas, marcos o procesos aceptados que rijan el uso de la IA en los sistemas, servicios y aplicaciones de salud, en particular en relación con la ética, la equidad, la seguridad, la integridad de los datos y la calidad de la atención?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(34,  'es', 'Sistemas seguros de retroalimentación del paciente',
+'Específicamente, ¿hay un sistema de retroalimentación del paciente seguro, disponible y accesible?');
+INSERT INTO i18n.health_indicator(indicator_id, language_id, name, definition) VALUES(35,  'es', 'Contribución de la salud digital en la gestión de la salud de la población',
+'¿Las iniciativas actuales de salud digital del país contribuyen a la elaboración de informes y la toma de decisiones sobre salud pública?');
+
+
+
+
+
+
+
+
