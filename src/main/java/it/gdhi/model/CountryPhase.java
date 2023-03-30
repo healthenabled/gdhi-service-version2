fd54ac89a564199949824d903398fbed8de5b646
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-03-30 11:10:58 +0530

    GDHI-76 Add updated_at column to country phase <Lahari/Arjun>

--- src/main/java/it/gdhi/model/CountryPhase.java
+++ src/main/java/it/gdhi/model/CountryPhase.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(schema = "country_health_data", name = "country_phase")
@@ -22,6 +23,8 @@ public class CountryPhase {
     @Column(name = "country_overall_phase")
     private Integer countryOverallPhase;
 
+    private Date updatedAt;
+
     public CountryPhase(String countryId, Integer countryOverallPhase, String year) {
         this.countryPhaseId = new CountryPhaseId(countryId, year);
         this.countryOverallPhase = countryOverallPhase;
@@ -29,5 +32,11 @@ public class CountryPhase {
 
     public String getYear() {
         return this.countryPhaseId.getYear();
+    }
+
+    @PreUpdate
+    @PrePersist
+    public void updateTimeStamps() {
+        updatedAt = new Date();
     }
 }
