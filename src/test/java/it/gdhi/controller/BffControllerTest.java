146064aa455439678d7585739f4d8afc5897bf6d
haikyuu77 <arjunsure.mec@gmail.com>
2023-03-30 15:47:41 +0530

    GDHI-76 Add contract for getting year on year data <Arjun/Lahari>

--- src/test/java/it/gdhi/controller/BffControllerTest.java
+++ src/test/java/it/gdhi/controller/BffControllerTest.java
@@ -1,5 +1,7 @@
 package it.gdhi.controller;
 
+import java.util.Collections;
+
 import it.gdhi.service.BffService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BffControllerTest {
@@ -22,5 +25,17 @@ public class BffControllerTest {
     public void shouldGetDistinctYears() {
         bffController.getDistinctYears();
         verify(bffService).fetchDistinctYears();
+    }
+    @Test
+    public void shouldGetYearOnYearData() {
+        bffController.getYearOnYearData(null, 1);
+        verify(bffService).fetchPublishedYearsForACountry(null, 1);
+        verify(bffService).fetchYearOnYearData(bffService.fetchPublishedYearsForACountry(null, 1), null);
+    }
+    @Test
+    public void shouldGetYearOnYearDataForFiveYearsWhenLimitIsNull() {
+        bffController.getYearOnYearData(null, null);
+        verify(bffService).fetchPublishedYearsForACountry(null, 5);
+        verify(bffService).fetchYearOnYearData(bffService.fetchPublishedYearsForACountry(null, 5), null);
     }
 }
