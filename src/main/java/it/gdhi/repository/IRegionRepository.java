383e0766e44db68213d26d24bccc33b260d10bd9
Kaveri Menoki <krmenoki@gmail.com>
2023-04-02 19:55:18 +0530

    GDHI-32 Add repository for region class <Ashutosh/Kaveri>

--- src/main/java/it/gdhi/repository/IRegionRepository.java
+++ src/main/java/it/gdhi/repository/IRegionRepository.java
@@ -1,11 +1,15 @@
 package it.gdhi.repository;
 
+import it.gdhi.model.Country;
 import it.gdhi.model.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IRegionRepository extends JpaRepository<Region, String> {
 
+    List<Region> findAll();
 }
 
