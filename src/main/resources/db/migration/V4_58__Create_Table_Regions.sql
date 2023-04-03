e6b8cd546176c03458bfb9a80e3f3dfaaebf9ba7
Kaveri Menoki <krmenoki@gmail.com>
2023-04-03 11:27:37 +0530

    GDHI-32 Rename migration scripts and add if not exist for the region and regions_countries tables <Kaveri/Ashutosh>

--- src/main/resources/db/migration/V4_58__Create_Table_Regions.sql
+++ src/main/resources/db/migration/V4_58__Create_Table_Regions.sql
@@ -1,4 +1,4 @@
-CREATE TABLE regions.region (
+CREATE TABLE IF NOT EXISTS regions.region (
     id VARCHAR PRIMARY KEY,
     region_name VARCHAR
 );
--- src/main/resources/db/migration/V4_59__Create_Table_Regions_Countries_Mapping.sql
+++ src/main/resources/db/migration/V4_59__Create_Table_Regions_Countries_Mapping.sql
@@ -1,4 +1,4 @@
-CREATE TABLE regions.regions_countries(region_id VARCHAR,
+CREATE TABLE IF NOT EXISTS regions.regions_countries(region_id VARCHAR,
      country_id VARCHAR, PRIMARY KEY (region_id, country_id),
      FOREIGN KEY(region_id) REFERENCES regions.region (id),
      FOREIGN KEY(country_id) REFERENCES master.countries (id)
