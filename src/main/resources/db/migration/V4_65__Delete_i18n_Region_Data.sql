52cebb15c5c437a3d37942c2138a013b560fd928
ashutoshroy <ashutoshroywork@gmail.com>
2023-04-03 19:22:22 +0530

    GDHI-32 Udate migration scripts for region translations <Ashutosh/Kaveri>

---
+++ src/main/resources/db/migration/V4_65__Delete_i18n_Region_Data.sql
@@ -0,0 +1,1 @@
+DELETE FROM i18n.region;
---
+++ src/main/resources/db/migration/V4_66__Insert_i18n_Region.sql
@@ -0,0 +1,29 @@
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('PAHO', 'es', 'Región Panamericana');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('PAHO', 'ar', 'منطقة عموم أمريكا');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('PAHO', 'fr', 'Région panaméricaine');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('PAHO', 'pt', 'Região Pan -Americana');
+
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EMRO', 'es', 'Regional mediterráneo oriental');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EMRO', 'ar', 'شرق البحر المتوسط الإقليمي');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EMRO', 'fr', 'Méditerranée orientale régionale');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EMRO', 'pt', 'Regional do Mediterrâneo Oriental');
+
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('AFRO', 'es', 'Región africana');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('AFRO', 'ar', 'المنطقة الأفريقية');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('AFRO', 'fr', 'Région africaine');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('AFRO', 'pt', 'Região Africana');
+
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EURO', 'es', 'Región europea');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EURO', 'ar', 'المنطقة الأوروبية');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EURO', 'fr', 'Région européenne');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('EURO', 'pt', 'Região européia');
+
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('WPRO', 'es', 'Región del Pacífico occidental');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('WPRO', 'ar', 'منطقة المحيط الهادئ الغربية');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('WPRO', 'fr', 'Région du Pacifique occidental');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('WPRO', 'pt', 'Região do Pacífico Ocidental');
+
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('SEARO', 'es', 'Región del sudeste asiático');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('SEARO', 'ar', 'منطقة جنوب شرق آسيا');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('SEARO', 'fr', 'Région d''Asie du Sud-Est');
+INSERT INTO i18n.region(region_id, language_id, region_name) VALUES('SEARO', 'pt', 'Região do Sudeste Asiático');
