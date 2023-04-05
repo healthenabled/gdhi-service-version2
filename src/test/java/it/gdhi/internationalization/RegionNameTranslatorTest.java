e6cb2c48f00b8b941e2d16d6b2e90a55db4360a7
ashutoshroy <ashutoshroywork@gmail.com>
2023-04-05 10:29:59 +0530

    GDHI-32 Add test cases for Region Translation. <Ashutosh/Kaveri>

---
+++ src/test/java/it/gdhi/internationalization/RegionNameTranslatorTest.java
@@ -0,0 +1,73 @@
+package it.gdhi.internationalization;
+
+import com.google.common.collect.ImmutableList;
+import it.gdhi.internationalization.repository.IRegionTranslationRepository;
+import it.gdhi.internationalization.service.RegionNameTranslator;
+import it.gdhi.model.Region;
+import it.gdhi.utils.LanguageCode;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class RegionNameTranslatorTest {
+
+    @InjectMocks
+    private RegionNameTranslator translator;
+    @Mock
+    private IRegionTranslationRepository translationRepository;
+
+    public Region createRegion(String id, String name) {
+        Region region = Region.builder().region_id(id).regionName(name).build();
+        return region;
+    }
+
+    @Test
+    public void shouldReturnRegionNamesInEnglishGivenUserLanguageIsNull() {
+        List<Region> expectedRegions = new ArrayList<>();
+        expectedRegions.add(createRegion("PAHO", "Pan American Region"));
+        List<Region> actualRegions = new ArrayList<>();
+
+        actualRegions = translator.translate(expectedRegions, null);
+
+        assertEquals(expectedRegions, actualRegions);
+    }
+
+    @Test
+    public void shouldNotInvokeTranslationRepositoryGivenUserLanguageIsNull() {
+        Region paho = createRegion("PAHO", "Pan American Region");
+        Region afro = createRegion("AFRO", "African Region");
+        List<Region> expectedRegions = ImmutableList.of(paho, afro);
+
+        translator.translate(expectedRegions, null);
+
+        verify(translationRepository, never()).findByIdRegionIdAndIdLanguageId(anyString(), anyString());
+    }
+
+
+    @Test
+    public void shouldReturnRegionNamesInFrenchGivenUserLanguageIdIsFr()
+    {
+        Region paho = createRegion("PAHO", "Région panaméricaine");
+        Region afro = createRegion("AFRO", "Région africaine");
+        List<Region> expectedRegions = ImmutableList.of(paho, afro);
+
+        when(translationRepository.findByIdRegionIdAndIdLanguageId("fr", paho.getRegion_id())).thenReturn("Région panaméricaine");
+        when(translationRepository.findByIdRegionIdAndIdLanguageId("fr",afro.getRegion_id())).thenReturn("Région africaine");
+        List<Region> actualRegions = translator.translate(expectedRegions, LanguageCode.fr);
+
+        assertEquals(expectedRegions, actualRegions);
+    }
+}
---
+++ src/test/java/it/gdhi/internationalization/repository/IRegionTranslationRepositoryTest.java
@@ -0,0 +1,28 @@
+package it.gdhi.internationalization.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@Transactional
+@ActiveProfiles("test")
+public class IRegionTranslationRepositoryTest {
+
+    @Autowired
+    private IRegionTranslationRepository repository;
+
+    @Test
+    public void shouldReturnRegionNameInSpanish() {
+        String region_name = repository.findByIdRegionIdAndIdLanguageId("PAHO", "es");
+        assertEquals(region_name, "Región Panamericana");
+    }
+}
