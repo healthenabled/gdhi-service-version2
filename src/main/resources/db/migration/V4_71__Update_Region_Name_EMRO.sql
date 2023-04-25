fc9942cc328d9b7393fdba551769caa34d656a46
ashutoshroy <ashutoshroywork@gmail.com>
2023-04-25 12:52:46 +0530

    GDHI-91 Add migration script to update region name. <Ashutosh/Lahari>

---
+++ src/main/resources/db/migration/V4_71__Update_Region_Name_EMRO.sql
@@ -0,0 +1,5 @@
+UPDATE regions.region SET region_name = 'Eastern Mediterranean Region' WHERE region_id='EMRO';
+UPDATE i18n.region SET region_name='Región del Mediterráneo Oriental' where region_id='EMRO' and language_id='es';
+UPDATE i18n.region SET region_name='إقليم شرق المتوسط' where region_id='EMRO' and language_id='ar';
+UPDATE i18n.region SET region_name='Região do Mediterrâneo Oriental' where region_id='EMRO' and language_id='pt';
+UPDATE i18n.region SET region_name='Région de la Méditerranée orientale' where region_id='EMRO' and language_id='fr';
