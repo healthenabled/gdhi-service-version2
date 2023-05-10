83e0fcf86d82c6988c45c7e9e4ee66e8bfcc8527
ashutoshroy <ashutoshroywork@gmail.com>
2023-05-10 10:10:23 +0530

    GDHI-153 Remove swagger dependency. <Ashutosh>

--- src/main/java/it/gdhi/GdhiServiceApplication.java
+++ src/main/java/it/gdhi/GdhiServiceApplication.java
@@ -26,10 +26,6 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.util.StringUtils;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -44,7 +40,6 @@ import java.util.Properties;
 @EnableJpaRepositories(basePackages = "it.gdhi")
 @ComponentScan(basePackageClasses = {GdhiServiceApplication.class})
 @EnableAsync
-@EnableSwagger2
 public class GdhiServiceApplication {
 
 	@Value("${db.url}")
@@ -87,11 +82,7 @@ public class GdhiServiceApplication {
 		jpaProperties.put("hibernate.event.merge.entity_copy_observer", "allow");
 		return jpaProperties;
 	}
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("it.gdhi")).build();
-	}
+
 	@Bean
 	public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
 																		 ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
